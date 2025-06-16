86c6f823f006be4f9e559f42b6fbaccd7914754f
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-06-16 16:03:38 +0500

    Add the two files in lib directory stateless and stateful

---
+++ b_state_less_and_full_widgets_components/lib/statefulwidgets.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(YellowBird());
+
+class YellowBird extends StatefulWidget {
+  const YellowBird({ super.key });
+
+  @override
+  State<YellowBird> createState() => _YellowBirdState();
+}
+
+class _YellowBirdState extends State<YellowBird> {
+  @override
+  Widget build(BuildContext context) {
+    // return Container(color: const Color(0xFFFFE306));
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello, World!"),
+        ),
+      ),
+    );
+  }
+}
