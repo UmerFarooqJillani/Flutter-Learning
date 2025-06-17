525daee9cf71f284d229830adba144cef4bf5b76
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-06-17 10:42:29 +0500

    Upadte the README.md

--- b_state_less_and_full_widgets_components/lib/statelesswidgets.dart
+++ b_state_less_and_full_widgets_components/lib/statelesswidgets.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,8 +12,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // Hide debug banner
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Umer Farooq Jillan'),
-          backgroundColor: Colors.indigo,
+          title: const Text('My Flutter App'),
+          // backgroundColor: Colors.indigo,
           leading: Icon(Icons.menu), // top-left icon
           actions: [
             IconButton(
@@ -23,13 +22,13 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-
+// ------------------------------------------------------------------
         drawer: Drawer( // Left-side drawer
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
               DrawerHeader(
-                decoration: BoxDecoration(color: const Color.fromARGB(255, 140, 157, 255)),
+                decoration: BoxDecoration(color: Colors.indigo),
                 child: Text('Menu', style: TextStyle(color: Colors.white)),
               ),
               ListTile(
@@ -43,24 +42,24 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-
+// ------------------------------------------------------------------
         endDrawer: Drawer( // Right-side drawer
           child: Center(child: Text("Right Drawer")),
         ),
-
+// ------------------------------------------------------------------
         body: Center(
           child: Text(
-            'Welcome to Umer!',
+            'Welcome to Flutter!',
             style: TextStyle(fontSize: 24),
           ),
         ),
-
+// ------------------------------------------------------------------
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          backgroundColor: Colors.indigo,
-          child: Icon(Icons.qr_code_scanner),
+          // backgroundColor: Colors.indigo,
+          child: Icon(Icons.add),
         ),
-
+// ------------------------------------------------------------------
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(
@@ -68,27 +67,23 @@ class MyApp extends StatelessWidget {
               label: 'Home',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.headphones),
-              label: 'Profile',
-            ),
-            BottomNavigationBarItem(
               icon: Icon(Icons.person),
               label: 'Profile',
             ),
           ],
         ),
-
+// ------------------------------------------------------------------
         bottomSheet: Container(
-          height: 100,
-          color: Colors.indigo,
+          height: 50,
+          color: Colors.yellow,
           child: Center(
             child: Text('Bottom Sheet',
-                style: TextStyle(color: Colors.white)),
+                style: TextStyle(color: Colors.black)),
           ),
         ),
-
-        backgroundColor: Colors.grey[200],
-
+// ------------------------------------------------------------------
+        backgroundColor: Colors.white,
+// ------------------------------------------------------------------
         resizeToAvoidBottomInset: true, // true = adjust for keyboard
       ),
     );
