67197dee889a35a6dbbcfae23aa387ca2bc440ff
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-06-27 16:16:18 +0500

    update the README.md and modify the home page to with scrollable list

--- d_media_query_splash_screen_other_ui_widgets_theme/README.md
+++ d_media_query_splash_screen_other_ui_widgets_theme/README.md
@@ -1,3 +1,45 @@
 # <p align="center"> d_media_query_splash_screen_other_ui_widgets_theme </p>
 
-## 
+## null-safety
+### `!` (Null assertion operator)
+- Ensures that currentState is not null. 
+- It's say the Dart Trust me — it’s not null.
+- Without it, Dart will complain if you access. validate() on something that could be null. 
+- `!`: force non-null (⚠️ will crash if value is null).
+### `??` (null-coalescing operator)
+- It returns the value on the left if it's not null, otherwise it returns the value on the right.
+- **?? '' means:** if value is null, use empty string.
+```dart
+List<Map<String, String>> songsList = [
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+  ];
+
+itemBuilder: (context, index) {
+    final song = songsList[index]; // Extract Map at index
+    return ListTile(
+        title: Text(song['name']), // Null-safe access
+        subtitle: Text(song['description'] ?? ''),
+        leading: Image.asset(song['path'] ?? '../assets/images/a.png'),
+    );
+},
+```
+- OR, In simple example:
+```dart
+    String? name;
+    print(name ?? 'Guest'); // Output: Guest
+//If name is null, it returns 'Guest'
+```
--- d_media_query_splash_screen_other_ui_widgets_theme/lib/pages/home_page.dart
+++ d_media_query_splash_screen_other_ui_widgets_theme/lib/pages/home_page.dart
@@ -2,6 +2,58 @@ import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
+  List<Map<String, String>> songsList = [
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'b',
+      'path': '../assets/images/b.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'c',
+      'path': '../assets/images/c.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+    {
+      'name': 'a',
+      'path': '../assets/images/a.png',
+      'description': 'click to more details',
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,10 +63,30 @@ class HomePage extends StatelessWidget {
       ),
       drawer: MyDrawer(),
       body: ListView.separated(
-          itemCount: 26,
-          itemBuilder: (context, index) => ListTile(title: Text("Hello")),
-          separatorBuilder: (context, index) => Divider(color: const Color.fromARGB(255, 216, 17, 17)),
-        ),
+        itemCount: 10,
+        // -------------------------------------------------
+        // preferred this
+        itemBuilder: (context, index) {
+          final song = songsList[index]; // Extract Map at index
+          return ListTile(
+            title: Text(song['name'] ?? ''), // Null-safe access
+            subtitle: Text(song['description'] ?? ''),
+            leading: Image.asset(song['path'] ?? '../assets/images/a.png'),
+          );
+        },
+        // -------------------------------------------------
+        // itemBuilder: (context, index) {
+        //   return ListTile(
+        //     // title: Text(songsList[index]['name'] ?? ''), // Null-safe access
+        //     title: Text(songsList[index]['name']!), // Null-safe access
+        //     subtitle: Text(songsList[index]['description']!),
+        //     leading: Image.asset(songsList[index]['path']!),
+        //   );
+        // },
+        // -----------------------------------------------------
+        separatorBuilder: (context, index) =>
+            Divider(color: const Color.fromARGB(106, 186, 186, 186)),
+      ),
     );
   }
 }
