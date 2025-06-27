86ccfbf116fc8b6019ee0adef5492c3e195927b3
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-06-27 12:39:56 +0500

    Add the Assets folder with images

--- d_media_query_splash_screen_other_ui_widgets_theme/lib/pages/home_page.dart
+++ d_media_query_splash_screen_other_ui_widgets_theme/lib/pages/home_page.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget{
-  
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AppBar"),
+        title: Text("Pandding"),
         // leading: Icon(Icons.menu),
       ),
       drawer: MyDrawer(),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(),
-          child: Text("My App"),
+      body: ListView.separated(
+          itemCount: 26,
+          itemBuilder: (context, index) => ListTile(title: Text("Hello")),
+          separatorBuilder: (context, index) => Divider(color: const Color.fromARGB(255, 216, 17, 17)),
         ),
-      ),
     );
   }
 }
