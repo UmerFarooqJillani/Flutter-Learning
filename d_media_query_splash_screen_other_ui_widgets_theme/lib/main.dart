e1737c4d56ed2a9f3657d5ec4866b4fc07774876
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-06-30 15:46:05 +0500

    Update README

--- d_media_query_splash_screen_other_ui_widgets_theme/lib/main.dart
+++ d_media_query_splash_screen_other_ui_widgets_theme/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import '/app/routes.dart';
-
+import 'app/themes.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   MyApp({super.key});
-   
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppTheme.darkTheme,
       initialRoute: '/',
       routes: AppRoutes.routes,
     );
   }
-}    
+}
