0645f318e70f792cd48ec3b2c8b18ba61140f6eb
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-07-11 12:22:55 +0500

    Separate the scrollable list in songs page

--- d_media_query_splash_screen_other_ui_widgets_theme/lib/features/home/home_screen.dart
+++ d_media_query_splash_screen_other_ui_widgets_theme/lib/features/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../data/alphabet_data.dart';
 import 'home_widgets.dart';
 import '../../widgets/bottom_menu/bottom_bar.dart';
 
@@ -24,56 +23,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: MyAppBar(),
       drawer: MyDrawer(),
-      body: ListView.separated(
-        itemCount: alphabetList.length,
-        itemBuilder: (context, index) {
-          final item = alphabetList[index];
-          return ListTile(
-            title: Text(
-              item.letter,
-              style: TextStyle(
-                // color: Colors.black
-              ),
-            ),
-            subtitle: Text(item.description),
-            leading: Image.asset(item.imagePath),
-            // trailing: ,   //visible on left of list item
-            onTap: () {
-              print("object: $index");
-              Navigator.pushNamed(context, '/song');
-            },
-          );
-        },
-        // -------------------------------------------------
-        // ListView.separated(
-        //   itemCount: songsList.length,
-        //   // -------------------------------------------------
-        //   // In this case preferred this ...
-        //   itemBuilder: (context, index) {
-        //     final song = songsList[index]; // Extract Map at index
-        //     return ListTile(
-        //       title: Text(song['name'] ?? '', style: TextStyle(
-        //         color: Colors.black
-        //       ),), // Null-safe access
-        //       subtitle: Text(song['description'] ?? ''),
-        //       leading: Image.asset(song['path'] ?? '../assets/images/a.png'),
-        //       onTap: () {
-        //         print("Hello $index");
-        //       },
-        //     );
-        //   },
-        // -------------------------------------------------
-        // itemBuilder: (context, index) {
-        //   return ListTile(
-        //     // title: Text(songsList[index]['name'] ?? ''), // Null-safe access
-        //     title: Text(songsList[index]['name']!), // Null-safe access
-        //     subtitle: Text(songsList[index]['description']!),
-        //     leading: Image.asset(songsList[index]['path']!),
-        //   );
-        // },
-        // -----------------------------------------------------
-        separatorBuilder: (context, index) =>
-            Divider(color: const Color.fromARGB(106, 186, 186, 186)),
+      body: Center(
+        child: Text("Not Design"),
       ),
       // --------------------------------------------------------------------------------------------
       // floatingActionButton: FloatingActionButton(
--- d_media_query_splash_screen_other_ui_widgets_theme/lib/features/songs/song_screen.dart
+++ d_media_query_splash_screen_other_ui_widgets_theme/lib/features/songs/song_screen.dart
@@ -1,12 +1,15 @@
+import 'package:d_media_query_splash_screen_other_ui_widgets_theme/data/alphabet_data.dart';
 import 'package:d_media_query_splash_screen_other_ui_widgets_theme/widgets/bottom_menu/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class SongPage extends StatelessWidget {
+  const SongPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // isSelectIndex -= 1;
+        // isSelectIndex -= 1; 
       ),
       // body: Container(
       //   height: 300,
@@ -53,10 +56,62 @@ class SongPage extends StatelessWidget {
       //   child: Container(width: 400, height: 200, color: Colors.red),
       // ),
       //---------------------------------------------------------------------------
-      body: Center(
-        child: Text("Not, Design Song Page"),
+      body: ListView.separated(
+        shrinkWrap: true,
+        // itemCount: alphabetList.length,
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          final item = alphabetList[index];
+          return ListTile(
+            title: Text(
+              item.letter,
+              style: TextStyle(
+                // color: Colors.black
+              ),
+            ),
+            subtitle: Text(item.description),
+            leading: Image.asset(item.imagePath),
+            // trailing: ,   //visible on left of list item
+            onTap: () {
+              print("object: $index");
+              Navigator.pushNamed(context, '/song');
+            },
+          );
+        },
+        // -------------------------------------------------
+        // ListView.separated(
+        //   itemCount: songsList.length,
+        //   // -------------------------------------------------
+        //   // In this case preferred this ...
+        //   itemBuilder: (context, index) {
+        //     final song = songsList[index]; // Extract Map at index
+        //     return ListTile(
+        //       title: Text(song['name'] ?? '', style: TextStyle(
+        //         color: Colors.black
+        //       ),), // Null-safe access
+        //       subtitle: Text(song['description'] ?? ''),
+        //       leading: Image.asset(song['path'] ?? '../assets/images/a.png'),
+        //       onTap: () {
+        //         print("Hello $index");
+        //       },
+        //     );
+        //   },
+        // -------------------------------------------------
+        // itemBuilder: (context, index) {
+        //   return ListTile(
+        //     // title: Text(songsList[index]['name'] ?? ''), // Null-safe access
+        //     title: Text(songsList[index]['name']!), // Null-safe access
+        //     subtitle: Text(songsList[index]['description']!),
+        //     leading: Image.asset(songsList[index]['path']!),
+        //   );
+        // },
+        // -----------------------------------------------------
+        separatorBuilder: (context, index) =>
+            Divider(color: const Color.fromARGB(106, 186, 186, 186)),
       ),
+      //---------------------------------------------------------------------------
       bottomNavigationBar: MyBottomBar(),
+      //---------------------------------------------------------------------------
       //---------------------------------------------------------------------------
     );
   }
